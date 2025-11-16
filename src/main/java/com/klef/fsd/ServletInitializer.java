4b1fec34ece204d62711aafec85a6533b455dfa0
2300031308 <162705885+2300031308@users.noreply.github.com>
2025-11-16 20:50:56 +0530

    Update ServletInitializer.java

--- src/main/java/com/klef/fsd/ServletInitializer.java
+++ src/main/java/com/klef/fsd/ServletInitializer.java
@@ -1,1 +1,13 @@
+package com.klef.fsd;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SdpProjectApplication.class);
+    }
+}
 
